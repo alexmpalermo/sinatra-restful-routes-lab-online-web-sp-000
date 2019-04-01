f03c6c26cb761c980cee0b60d6ab9f50bbbbc8b2
Alexandra Palermo <alexmpalermo@gmail.com>
2019-04-01 21:00:30 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,14 +13,14 @@ class ApplicationController < Sinatra::Base
     erb :new
   end 
 
+
+
   post '/recipes' do
     @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
     redirect to "/recipes/#{@recipe.id}"
   end 
 
-  get '/recipes/:id' do 
-     @recipe = Recipe.find_by(params[:id])
-      erb :show 
+  
 end
 
   get '/recipes/:id/edit' do
