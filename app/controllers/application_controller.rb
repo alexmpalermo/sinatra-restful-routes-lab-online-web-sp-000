b64e1fa966650bdb7db30ecc0bf19a5f24710dfa
Alexandra Palermo <alexmpalermo@gmail.com>
2019-04-01 20:24:10 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,48 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes' do 
+    erb :index 
+  end
+  
+  get '/recipes/new' do
+    erb :new
+  end 
 
+  post '/recipes' do
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    redirect '/recipes/#{@recipe.id}'
+  end 
+
+  get '/recipes/:id' do 
+    if recipe = Recipe.find_by(params[:id])
+      erb :show 
+    else 
+      redirect '/recipes'
+  end
+end
+
+  get '/recipes/:id/edit' do
+    if @recipe = Recipe.find_by_id(params[:id])
+      erb :edit 
+    else 
+      redirect '/recipes'
+  end
+  end
+  
+  patch '/recipes/:id' do 
+  @recipe = Recipe.find_by_id(params[:id])
+  @recipe.name = params[:name]
+  @recipe.ingredients = params[:ingredients]
+  @recipe.cook_time = params[:cook_time]
+  @recipe.save
+  redirect '/recipes/#{@recipe.id}'
+end
+  
+  delete '/recipes/:id/delete' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect '/recipes'
+  end
+  
 end
